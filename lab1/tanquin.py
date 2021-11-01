5d0be3f9e63d42dca270155cc3d420ae97ca9bd2
Fermento99 <pk.pawlak99@gmail.com>
2021-11-01 22:01:23 +0100

    lab1 v2

--- lab1/tanquin.py
+++ lab1/tanquin.py
@@ -8,7 +8,7 @@ from utils import shuffle
 
 
 def printResult(visited: dict, currState: Board, display=0):
-    parent = visited[currState.frags]
+    parent = list(filter(lambda entr: entr[0] == currState.frags, visited))[0][1]
     if display == 2:
         print(currState)
     elif display == 1:
@@ -17,7 +17,7 @@ def printResult(visited: dict, currState: Board, display=0):
     path = 0
     while parent != (0, 0):
         currState = Board(parent)
-        parent = visited[currState.frags]
+        parent = list(filter(lambda entr: entr[0] == currState.frags, visited))[0][1]
         if display == 2:
             print(currState)
         elif display == 1:
@@ -29,19 +29,19 @@ def printResult(visited: dict, currState: Board, display=0):
 def astar(objective: list[int], heuristic: callable):
     end = Board(objective)
     start = Board(tuple([i for i in range(Board.size**2)]))
-    open = {start.frags: (start, (0, 0))}
-    closed = {}
+    open = [(start, (0, 0))]
+    closed = []
 
     # print(end)
 
     while len(open) > 0:
-        # if len(closed) % 1_000 == 0:
-        #     print('closed: {:.3f}; open: {:.3f}'.format(len(closed)/1_000_000, len(open)/1_000_000), end='\r')
+        if len(closed) % 1_000 == 0:
+            print('closed: {:.3f}; open: {:.3f}'.format(len(closed)/1_000_000, len(open)/1_000_000), end='\r')
 
-        currState, parent = min(open.values(), key=lambda entr: entr[0].getCombinedDist())
+        currState, parent = min(open, key=lambda entr: entr[0].getCombinedDist())
 
-        open.pop(currState.frags)
-        closed[currState.frags] = parent
+        open.remove((currState, parent))
+        closed.append((currState.frags, parent))
 
         if currState.frags == end.frags:
             print('\t\t\t\t', end='\r')
@@ -54,18 +54,18 @@ def astar(objective: list[int], heuristic: callable):
             state = currState.fragsAfterMove(move)
             child = Board(state)
 
-            if child.frags in closed.keys():
+            if child.frags in map(lambda entr: entr[0], closed):
                 continue
 
             child.jumpDist = currState.jumpDist + 1
             child.heurDist = heuristic(child.frags, end.frags)
 
-            if child.frags in open.keys():
-                if open[child.frags][0].getCombinedDist() > child.getCombinedDist():
-                    open[child.frags] = (child, currState.frags)
-                continue
-            
-            open[child.frags] = (child, currState.frags)
+            try:
+                index = list(map(lambda entr: entr[0].frags, open)).index(child.frags)
+                if open[index][0].getCombinedDist() > child.getCombinedDist():
+                    open[index] = (child, currState.frags)
+            except ValueError:
+                open.append((child, currState.frags))
 
 
 def test(boardCount: int, size: int, randomMoves=0, shuffleMoves=0, manHeu=False, wellHeu=False, noHeu=False ):
@@ -97,7 +97,7 @@ def test(boardCount: int, size: int, randomMoves=0, shuffleMoves=0, manHeu=False
 
 
 def main():
-    test(10, 3, randomMoves=30, manHeu=True, wellHeu=True)
+    test(1, 4, shuffleMoves=1, manHeu=True, wellHeu=True)
 
 if __name__ == "__main__":
     main()
